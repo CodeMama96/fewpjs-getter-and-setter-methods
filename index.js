aa5d6e2e401ee5759c9fcf17a921d9ba14335ace
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-12 14:09:02 -0500

    commit tree 8de9ed968a50613950f9e53f10bd517c88b9ccee

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your Circle class here
+
+//circle = new Circle( 6 )
+
+
+
+
+class Circle {
+    constructor(radius){
+        this.radius = radius;
+    }
+
+    get diameter(){
+        return this.radius * 2
+    }
+
+    set diameter(diameter){
+        this.radius = diameter / 2
+        //this._diameter = diameter
+    }
+
+    get circumference(){
+        return 2 * Math.PI * this.radius;
+    }
+
+    set circumference(circumference){
+        //this._circumference = circumference
+        this.radius = circumference / ( Math.PI * 2 )
+    }
+
+    get area(){
+        //return this.radius**2(Math.PI)
+        return Math.PI * this.radius * this.radius;
+    }
+}
